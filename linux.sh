a6aa92464aad6c153bb416c4b2b0de760e9a78f7
Kim <Kimsia478@gmail.com>
2022-12-05 19:59:58 +0000

    name changed

---
+++ linux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Please enter your name"
+read name
+echo "Reading...please wait"
+sleep 3
+echo "----------------------"
+echo "Your username is:" &&whoami
+echo "----------------------"
+echo "This is one of my first Linux scripts"
+sleep 2 
+echo "Please wait while we process the date....."
+sleep 3
+echo "Made on:" && date
+echo "--------------------"
+sleep 2
+echo "My Ip address is:"
+echo "--------------------"
+sleep 2
+
+curl ifconfig.me
+
+This is week 4-project test 
